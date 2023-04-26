76d91c0cf55e37a44dc75207455be9c2dad11778
Forrest Yankey <129310337+ForrestYankey@users.noreply.github.com>
2023-04-26 11:39:07 -0400

    Finish fucntion getComputerNumber

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // computer picks a number between 1-3
+function getComputerChoice() {
+    num = Math.floor(Math.random() * 3) + 1;
+    console.log(num);
 // if 1, return "Rock"
-    if (num === 1) {
-        return computerChoice = "Rock";
-    }   
+    if (num == 1) {
+       return getComputerChoice = "Rock";
 // if else 2, return "Paper"
-    else if (num === 2) {
-        return computerChoice = "Papaer";
-    }
+   }   else if (num == 2) {   
+            return getComputerChoice = "Paper";
 // else, return "Scissors"
-    else {
-        return computerChoice = "Scissors";
-    }
-    console.log(computerChoice)
+   } else {   
+            return getComputerChoice = "Scissors";
+   }
+}
+   
+console.log(getComputerChoice());
