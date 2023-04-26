975ae5e72ad5e9db5c76dda9b4e35f87a1ee4d3b
Forrest Yankey <129310337+ForrestYankey@users.noreply.github.com>
2023-04-26 14:39:58 -0400

    Edit script.js

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 // computer picks a number between 1-3
-function computerChoice() {
+function getComputerSelection() {
     num = Math.floor(Math.random() * 3) + 1;
 // if 1, return "Rock"
     if (num == 1) {
-       return computerChoice = "Rock";
+       return getComputerSelection = "Rock";
 // if else 2, return "Paper"
    }   else if (num == 2) {   
-            return computerChoice = "Paper";
+            return getComputerSelection = "Paper";
 // else, return "Scissors"
    } else {   
-            return computerChoice = "Scissors";
+            return getComputerSelection = "Scissors";
    }
 }
 
 // ask player for choice
-let playerChoice = prompt("Rock, Paper, or Scissors?");
-// compare getPlayerChoice to getComputerChoice
-function gameResults() {
-// if getPlayerChoice == getComputerChoice return "Tie!"
-    if (playerChoice === computerChoice) {
-        return gameResults = "It's a tie!";
-// if getPlayerChoice == Rock and getComputerChoice == Paper return "Computer wins!"
-}   else if ((playerChoice = "Rock") && (computerChoice = "Paper")) {
-        return gameResults = "Computer wins!";
-// if getPlayerChoice == Paper and getComputerChoice == Rock return "You win!"
-}   else if ((playerChoice = "Paper") && (computerChoice = "Rock")) {
-return gameResults = "You win!";
-// if getPlayerChoice == Paper and getComputerChoice == Scissors return "Computer wins!"
-}   else if ((playerChoice = "Paper") && (computerChoice = "Scissors")) {
-    return gameResults = "Computer wins!";
-// if getPlayerChoice == Scissors and getComputerChoice == Paper return "You win!"
-}   else if ((playerChoice = "Scissors") && (computerChoice = "Paper")) {
-    return gameResults = "You win!";
-// if getPlayerChoice == Rock and getComputerChoice == Scissors return "You win!"
-}   else if ((playerChoice = "Rock") && (computerChoice = "Scissors")) {
-    return gameResults = "You win!";
-// if getPlayerChoice == Scissors and getComputerChoice == Rock return "Computer wins!"
-}   else if ((playerChoice = "Scissors") && (computerChoice = "Rock")) {
-    return gameResults = "Computer wins!";
+let getPlayerChoice = prompt("Rock, Paper, or Scissors?");
+// compare getgetPlayerChoice to getgetComputerSelection
+function playRound() {
+// if getgetPlayerChoice == getgetComputerSelection return "Tie!"
+    if (getPlayerChoice === getComputerSelection) {
+        return playRound = "It's a tie!";
+// if getgetPlayerChoice == Rock and getgetComputerSelection == Paper return "Computer wins!"
+}   else if ((getPlayerChoice = "Rock") && (getComputerSelection = "Paper")) {
+        return playRound = "Computer wins!";
+// if getgetPlayerChoice == Paper and getgetComputerSelection == Rock return "You win!"
+}   else if ((getPlayerChoice = "Paper") && (getComputerSelection = "Rock")) {
+return playRound = "You win!";
+// if getgetPlayerChoice == Paper and getgetComputerSelection == Scissors return "Computer wins!"
+}   else if ((getPlayerChoice = "Paper") && (getComputerSelection = "Scissors")) {
+    return playRound = "Computer wins!";
+// if getgetPlayerChoice == Scissors and getgetComputerSelection == Paper return "You win!"
+}   else if ((getPlayerChoice = "Scissors") && (getComputerSelection = "Paper")) {
+    return playRound = "You win!";
+// if getgetPlayerChoice == Rock and getgetComputerSelection == Scissors return "You win!"
+}   else if ((getPlayerChoice = "Rock") && (getComputerSelection = "Scissors")) {
+    return playRound = "You win!";
+// if getgetPlayerChoice == Scissors and getgetComputerSelection == Rock return "Computer wins!"
+}   else if ((getPlayerChoice = "Scissors") && (getComputerSelection = "Rock")) {
+    return playRound = "Computer wins!";
 }
 }
 // Display outcome
-console.log(computerChoice() + ":computer")
-console.log(playerChoice + ":player")
-console.log(gameResults())
+
+//console.log("computer: " + getComputerSelection())
+//console.log("player: " + getPlayerChoice)
+//console.log(playRound())
+
+const playerSelection = getPlayerChoice;
+const computerSelection = getComputerSelection();
+//run the getComputerSelection vs getPlayerChoice five time in a row and show the results each time
+function game() {
+    return game = playRound(playerSelection, computerSelection)
+}
+console.log("computer: " + computerSelection)
+console.log("player: " + playerSelection)
+console.log(game())
+
+
+
