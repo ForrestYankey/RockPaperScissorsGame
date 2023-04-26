6e47d65f5ea9431f5ef00333287268e6fefba402
Forrest Yankey <129310337+ForrestYankey@users.noreply.github.com>
2023-04-26 11:08:12 -0400

    Add code to pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+// computer picks a number between 1-3
+// if 1, return "Rock"
+    if (num === 1) {
+        return computerChoice = "Rock";
+    }   
+// if else 2, return "Paper"
+    else if (num === 2) {
+        return computerChoice = "Papaer";
+    }
+// else, return "Scissors"
+    else {
+        return computerChoice = "Scissors";
+    }
+    console.log(computerChoice)
